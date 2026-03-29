fbe00e42dd6f77503e2f57d6fbfb361c2066f4b1
kotle9ka <popovkolya81010@gmail.com>
2026-03-29 14:27:13 +0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/book-landing-page/"
+  base: "/goit-react-hw-02/"
 })
